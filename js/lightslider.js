543db633c273898e8f53ae63efcebefb7d78fd0c
WindRMen <windrmeno5@gmail.com>
2021-02-27 18:53:24 +0530

    Update lightslider.js

--- js/lightslider.js
+++ js/lightslider.js
@@ -4,17 +4,17 @@
 (function ($, undefined) {
     'use strict';
     var defaults = {
-        item: 3,
+        item: 1,
         autoWidth: false,
         slideMove: 1,
-        slideMargin: 20,
+        slideMargin: 5,
         addClass: '',
         mode: 'slide',
         useCSS: true,
         cssEasing: 'ease-out', //'cubic-bezier(0.25, 0, 0.25, 1)',
         easing: 'linear', //'for jquery animation',//
         speed: 400, //ms'
-        auto: true,
+        auto: false,
         pauseOnHover: false,
         loop: true,
         slideEndAnimation: true,
@@ -28,16 +28,17 @@
         vertical: false,
         verticalHeight: 500,
         vThumbWidth: 100,
+        // centerSlide:true,
         thumbItem: 10,
         pager: false,
         gallery: false,
         galleryMargin: 5,
         thumbMargin: 5,
-        currentPagerPosition: 'middle',
+        currentPagerPosition: 'left',
         enableTouch: true,
         enableDrag: true,
         freeMove: true,
-        swipeThreshold: 50,
+        swipeThreshold: 40,
         responsive: [],
         /* jshint ignore:start */
         onBeforeStart: function ($el) {},
@@ -48,6 +49,32 @@
         onBeforePrevSlide: function ($el, scene) {}
         /* jshint ignore:end */
     };
+
+//////////////////////////////
+//     if (settings.rtl === true) {
+//         $slide.parent().addClass('lSrtl');
+//     }
+//     if (settings.centerSlide === true) {
+//         $slide.parent().addClass('lScenterSlide');
+//     }
+//     if (settings.vertical) {
+//         $slide.parent().addClass('vertical');
+//         elSize = settings.verticalHeight;
+//  -519,6 +523,9 
+//     }
+// },
+// move: function (ob, v) {
+//     if (settings.centerSlide === true) {
+//         v -= (windowW - slideWidth) / 2;
+//     }
+//     if (settings.rtl === true) {
+//         v = -v;
+//     }
+//  -1076,4 +1083,4 
+// });
+// return this;
+/////////////////////////////////
+
     $.fn.lightSlider = function (options) {
         if (this.length === 0) {
             return this;
