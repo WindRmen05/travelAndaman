a20f45604f76d7675d15f8c9ef00b00e871c4cc9
WindRMen <windrmeno5@gmail.com>
2021-03-04 18:48:36 +0530

    new updates

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+var button = document.querySelector('.more-button');
+const text = document.querySelector('.text');
+const moretext = document.querySelector('.moretext');
+const spanColor = document.querySelector('.span-color');
+
+button.addEventListener('click',(e) => {
+    moretext.classList.toggle('moretext');
+    if (button.textContent.includes('Read More')) {
+        button.textContent='Read less';
+        setTimeout(() => {
+            spanColor.style.background = 'transparent';
+        },2000);
+    } else {
+        button.textContent='Read More';
+        spanColor.style.background = 'antiquewhite';
+    }
+})
+// console.log(button[0].textContent);
