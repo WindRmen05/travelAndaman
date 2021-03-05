31f0858f15e94ec104020e56e68bfc7cd8bb4e12
WindRMen <windrmeno5@gmail.com>
2021-03-05 15:39:03 +0530

    copyright added

--- js/script.js
+++ js/script.js
@@ -7,15 +7,3 @@ $(document).ready(function() {
         } 
     });  
 });
-
-
-/////////////////////
-
-// $(document).ready(function(){
-//     $(".navigation__item").click(function(){
-//         $(input[type="checkbox"]).prop("checked", false);
-//     });
-//     // $(".uncheck").click(function(){
-//     //     $("#myCheck").prop("checked", false);
-//     // });
-// });
