cb4a15c6ab11d9d052b2c510054890f0d06f8a77
WindRMen <windrmeno5@gmail.com>
2021-03-25 15:42:50 +0530

    nav arrows added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:false,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-});
+// $(document).ready(function() {
+//     $('.slick-slider').lightSlider({
+//         autoWidth:false,
+//         loop:true,
+//         arrows: true,
+//         onSliderLoad: function() {
+//             $('.slick-slider').removeClass('cS-hidden');
+//         } 
+//     });  
+// });
 
-          
+$('.slick-slider').slick({
+    infinite: true,
+    slidesToShow: 3, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    arrows: true, // Adds arrows to sides of slider
+    dots: true // Adds the dots on the bottom
+  });
